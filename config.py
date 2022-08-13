0e75a2eba9ee65e25d40994c481db6c8cfca99b8
KevinWjq <111183760+KevinWjq@users.noreply.github.com>
2022-08-14 01:59:23 +0800

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ app_secret = "a492a216816970e30b79ec0ba9fb62e4"
 # 模板消息id
 template_id = "KdKcfu-AmxRd-zxvd8FqFrHwubCRU6gbkoDJ7zf4sog"
 # 接收公众号消息的微信号，如果有多个，需要用英文逗号间隔
-user = ["onKwE56icCNcFE2F8nopbzfZAyPc"]
+user = ["onKwE532buSkQ5X4e-rCIuiPD-Fw"]
 
 # 信息配置
 # 所在省份
