0faaae04c61d7dcdfbd1e22f335d632818897489
KevinWjq <111183760+KevinWjq@users.noreply.github.com>
2022-08-14 01:20:24 +0800

    Update main.py

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ def get_ciba():
     return note_ch, note_en
 
 
-def send_message(to_user, access_token, city_name, weather, max_temperature, min_temperature, note_ch, note_en):
+def send_message(users, access_token, city_name, weather, max_temperature, min_temperature, note_ch, note_en):
     url = "https://api.weixin.qq.com/cgi-bin/message/template/send?access_token={}".format(access_token)
     week_list = ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"]
     year = localtime().tm_year
@@ -88,7 +88,7 @@ def send_message(to_user, access_token, city_name, weather, max_temperature, min
         birth_date = year_date
         birth_day = str(birth_date.__sub__(today)).split(" ")[0]
     data = {
-        "touser": to_user,
+        "touser": users,
         "template_id": os.environ["TEMPLATE_ID"],
         "url": "http://weixin.qq.com/download",
         "topcolor": "#FF0000",
