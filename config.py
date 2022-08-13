1ee5ed55d3947cff08513b87a85ed65cb09e6c93
KevinWjq <111183760+KevinWjq@users.noreply.github.com>
2022-08-13 21:20:37 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,20 +1,20 @@
 # 公众号配置
 # 公众号appId
-app_id = ""
+app_id = "wx25668ea48ba88a1c"
 # 公众号appSecret
-app_secret = ""
+app_secret = "a492a216816970e30b79ec0ba9fb62e4"
 # 模板消息id
-template_id = ""
+template_id = "KdKcfu-AmxRd-zxvd8FqFrHwubCRU6gbkoDJ7zf4sog"
 # 接收公众号消息的微信号，如果有多个，需要用英文逗号间隔
-user = [""]
+user = ["onKwE56icCNcFE2F8nopbzfZAyPc"]
 
 # 信息配置
 # 所在省份
-province = "广东"
+province = "上海"
 # 所在城市
-city = "广州"
+city = "上海"
 # 生日，如果月份或者日期小于10，直接用对应的数字即可，例如1997-1-1
-birthday = "1997-1-1"
+birthday = "1994-8-8"
 # 在一起的日子，格式同上
-love_date = "2022-1-1"
+love_date = "2022-1-15"
 
